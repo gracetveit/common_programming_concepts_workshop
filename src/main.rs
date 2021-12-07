dd54cd19bf1ae79c94a8d172429a5514ff5cacfb
Grace Tveit <grace.tveit@gmail.com>
2021-12-07 15:42:27 -0800

    docs: outlining assignments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// Convert between Fahrenheit and Celsius
+// Generate the nth Fibonacci number
+// Print the lyrics to "The Twelve Days of Christmas"
